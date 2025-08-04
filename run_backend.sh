d6cff4af4190614625b6b5cc6716a76d44f17f6c
Jaiden Reddy <jaidenreddy@gmail.com>
2025-08-04 13:10:37 -0700

    deleted

--- run_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source new_backend_ruminate/my_env/bin/activate
-
-# Run the uvicorn server
-PYTHONPATH=. uvicorn new_backend_ruminate.main:app --reload --port 8000
